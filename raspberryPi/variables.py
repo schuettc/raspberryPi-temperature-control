1bea6fcf07e82b6b69f1a6aabab18e3e9b7c5b1f
Court Schuett <schuettc@amazon.com>
2021-02-18 00:37:58 -0600

    add backend

--- raspberryPi/variables.py
+++ raspberryPi/variables.py
@@ -1,9 +1,10 @@
-SENSOR=["28-01204f79c6d6"]
-CLIENT_ID="testDevice"
-ENDPOINT="a1dpf39ivechwj-ats.iot.us-east-1.amazonaws.com"
-PATH_TO_CERT="certs/device.pem.crt"
-PATH_TO_KEY="certs/private.pem.key"
-PATH_TO_ROOT="certs/AmazonRootCA1.pem"
-TOPIC="iot/temperature"
-SENSORPATH="/sys/bus/w1/devices"
-SUFFIX="w1_slave"
+SENSOR=["28-1111111111111"]              # Name of directory in the /sys/bus/w1/devices folder
+SENSORPATH="/sys/bus/w1/devices"        # Directory sensor is located in
+SUFFIX="w1_slave"                       # File name for sensor
+
+CLIENT_ID="xxxxxxx"                  # Name of the client create in AWS-IoT
+ENDPOINT="xxxxxxxxxxxxxxx"   # Endpoing created in AWS-IoT
+PATH_TO_CERT="certs/device.pem.crt"     # The cert file created in AWS-IoT
+PATH_TO_KEY="certs/private.pem.key"     # The private key file created in AWS-IoT
+PATH_TO_ROOT="certs/AmazonRootCA1.pem"  # The AWS Root CA file available here: https://www.amazontrust.com/repository/AmazonRootCA1.pem
+TOPIC="iot/temperature"                 # Topic created in AWS-IoT
